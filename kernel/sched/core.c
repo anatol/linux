305205322a1b39ad2c73a5c5ecf604bea09262d8
Andrey Konovalov <adech.fo@gmail.com>
2014-10-08 18:27:17 +0400

    Fix typos

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2785,7 +2785,7 @@ asmlinkage __visible void schedule_tail(struct task_struct *prev)
 
 	rq = finish_task_switch(prev);
 
-	/* Restore thr->cpu in case it was zeroed in finish_tabk_switch. */
+	/* Restore thr->cpu in case it was zeroed in finish_task_switch. */
 	ktsan_thr_start();
 
 	balance_callback(rq);
