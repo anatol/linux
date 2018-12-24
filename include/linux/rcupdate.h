google/ktsan

Fix rcu interceptors

include/linux/rcupdate.h
@@ -581,32 +581,17 @@ static inline void rcu_preempt_sleep_check(void)
 	rcu_dereference_sparse(p, space); \
 	((typeof(*p) __force __kernel *)(_________p1)); \
 })
-
-#ifndef CONFIG_KTSAN
 #define __rcu_dereference_check(p, c, space) \
 ({ \
 	typeof(*p) *_________p1 = (typeof(*p) *__force)ACCESS_ONCE(p); \
 	rcu_lockdep_assert(c, "suspicious rcu_dereference_check() usage"); \
 	rcu_dereference_sparse(p, space); \
 	smp_read_barrier_depends(); /* Dependency order vs. p above. */ \
+	/* hlist_bl_set_first_rcu changes the last bit of the pointer by \
+	   applying LIST_BL_LOCKMASK. Do & ~7 to ignore that. */ \
+	ktsan_sync_acquire((void *)((unsigned long)_________p1 & ~7)); \
 	((typeof(*p) __force __kernel *)(_________p1)); \
 })
-#else /* CONFIG_KTSAN */
-#define __rcu_dereference_check(p, c, space) \
-({ \
-	typeof(*p) *_________p1; \
-	/* FIXME(xairy): remove when atomics and barriers are intercepted. */ \
-	ktsan_report_disable(); \
-	_________p1 = (typeof(*p) *__force)ACCESS_ONCE(p); \
-	rcu_lockdep_assert(c, "suspicious rcu_dereference_check() usage"); \
-	rcu_dereference_sparse(p, space); \
-	smp_read_barrier_depends(); /* Dependency order vs. p above. */ \
-	ktsan_report_enable(); \
-	ktsan_sync_acquire((void *)(p)); \
-	((typeof(*p) __force __kernel *)(_________p1)); \
-})
-#endif /* CONFIG_KTSAN */
-
 #define __rcu_dereference_protected(p, c, space) \
 ({ \
 	rcu_lockdep_assert(c, "suspicious rcu_dereference_protected() usage"); \
@@ -686,12 +671,12 @@ static inline void rcu_preempt_sleep_check(void)
 #else /* CONFIG_KTSAN */
 #define rcu_assign_pointer(p, v)				\
 	do {							\
-		/* FIXME(xairy): remove when atomics and	\
-		   barriers are intercepted. */			\
-		ktsan_report_disable();				\
+		/* hlist_bl_set_first_rcu changes the last bit	\
+		   of the pointer by applying LIST_BL_LOCKMASK.	\
+		   Do & ~7 to ignore that. */			\
+		ktsan_sync_release(				\
+			(void *)((unsigned long)(v) & ~7));	\
 		smp_store_release(&p, RCU_INITIALIZER(v));	\
-		ktsan_report_enable();				\
-		ktsan_sync_release((void *)(v));		\
 	} while (0)
 #endif /* CONFIG_KTSAN */
 

anatol/linux

include/linux/rcupdate.h
@@ -323,29 +323,17 @@ static inline void rcu_preempt_sleep_check(void) { }
 	((typeof(*p) __force __kernel *)(_________p1)); \
 })
 
-#ifndef CONFIG_KTSAN
 #define __rcu_dereference_check(p, c, space) \
 ({ \
 	/* Dependency order vs. p above. */ \
 	typeof(*p) *________p1 = (typeof(*p) *__force)READ_ONCE(p); \
 	RCU_LOCKDEP_WARN(!(c), "suspicious rcu_dereference_check() usage"); \
 	rcu_dereference_sparse(p, space); \
+	/* hlist_bl_set_first_rcu changes the last bit of the pointer by \
+	   applying LIST_BL_LOCKMASK. Do & ~7 to ignore that. */ \
+	ktsan_sync_acquire((void *)((unsigned long)________p1 & ~7)); \
 	((typeof(*p) __force __kernel *)(________p1)); \
 })
-#else /* CONFIG_KTSAN */
-#define __rcu_dereference_check(p, c, space) \
-({ \
-	typeof(*p) *________p1; \
-	/* FIXME(xairy): remove when atomics and barriers are intercepted. */ \
-	ktsan_report_disable(); \
-	________p1 = (typeof(*p) *__force)lockless_dereference(p); \
-	rcu_lockdep_assert(c, "suspicious rcu_dereference_check() usage"); \
-	rcu_dereference_sparse(p, space); \
-	ktsan_report_enable(); \
-	ktsan_sync_acquire((void *)(p)); \
-	((typeof(*p) __force __kernel *)(________p1)); \
-})
-#endif /* CONFIG_KTSAN */
 
 #define __rcu_dereference_protected(p, c, space) \
 ({ \
@@ -413,18 +401,16 @@ static inline void rcu_preempt_sleep_check(void) { }
 ({									      \
 	uintptr_t _r_a_p__v = (uintptr_t)(v);				      \
 									      \
-	/* FIXME(xairy): remove when atomics and	\
-	   barriers are intercepted. */			\
-	ktsan_report_disable();				\
+	/* hlist_bl_set_first_rcu changes the last bit	\
+	   of the pointer by applying LIST_BL_LOCKMASK.	\
+	   Do & ~7 to ignore that. */			\
+	ktsan_sync_release(				\
+		(void *)((unsigned long)(v) & ~7));	\
 	\
 	if (__builtin_constant_p(v) && (_r_a_p__v) == (uintptr_t)NULL)	      \
 		WRITE_ONCE((p), (typeof(p))(_r_a_p__v));		      \
 	else								      \
 		smp_store_release(&p, RCU_INITIALIZER((typeof(p))_r_a_p__v)); \
-	\
-	ktsan_report_enable();				\
-	ktsan_sync_release((void *)(v));			\
-	\
 	_r_a_p__v;							      \
 })
 #endif