google/ktsan

Annotate smp_store_release/smp_load_acquire

arch/x86/include/asm/barrier.h
@@ -84,14 +84,14 @@
 #define smp_read_barrier_depends()	ktsan_membar_acquire()
 #endif
 
+#ifndef CONFIG_KTSAN
+
 #if defined(CONFIG_X86_PPRO_FENCE)
 
 /*
  * For this option x86 doesn't have a strong TSO memory
  * model and we should fall back to full barriers.
  */
-
-/* TODO(xairy). */
 
 #define smp_store_release(p, v)						\
 do {									\
@@ -126,6 +126,29 @@ do {									\
 })
 
 #endif
+
+#else /* CONFIG_KTSAN */
+
+#define smp_store_release(p, v)						\
+do {									\
+	typeof(p) ___p1 = (p);						\
+	compiletime_assert_atomic_type(*___p1);				\
+	ktsan_sync_release((void *)___p1);				\
+	asm volatile("mfence":::"memory");				\
+	ACCESS_ONCE(*___p1) = (v);					\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(p) ___p1 = (p);						\
+	typeof(*p) ___p2 = ACCESS_ONCE(*___p1);				\
+	compiletime_assert_atomic_type(*___p1);				\
+	asm volatile("mfence":::"memory");				\
+	ktsan_sync_acquire((void *)___p1);				\
+	___p2;								\
+})
+
+#endif /* CONFIG_KTSAN */
 
 /* Atomic operations are already serializing on x86 */
 #ifndef CONFIG_KTSAN

anatol/linux

arch/x86/include/asm/barrier.h
@@ -97,6 +97,8 @@ static inline unsigned long array_index_mask_nospec(unsigned long index,
 #endif
 #define __smp_store_mb(var, value) do { (void)xchg(&var, value); } while (0)
 
+#ifndef CONFIG_KTSAN
+
 #define __smp_store_release(p, v)					\
 do {									\
 	compiletime_assert_atomic_type(*p);				\
@@ -111,6 +113,27 @@ do {									\
 	barrier();							\
 	___p1;								\
 })
+
+#else /* CONFIG_KTSAN */
+
+#define __smp_store_release(p, v)					\
+do {									\
+	compiletime_assert_atomic_type(*p);				\
+	ktsan_sync_release((void *)p);		\
+	barrier();							\
+	WRITE_ONCE(*p, v);						\
+} while (0)
+
+#define __smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = READ_ONCE(*p);				\
+	compiletime_assert_atomic_type(*p);				\
+	barrier();							\
+	ktsan_sync_acquire((void *)p);		\ 
+	___p1;								\
+})
+
+#endif /* CONFIG_KTSAN */
 
 /* Atomic operations are already serializing on x86 */
 #ifndef CONFIG_KTSAN